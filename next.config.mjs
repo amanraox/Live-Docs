4f36d757f5c1077d5cedb694cda166655a68e8b9
Aman Umrao <67886237+amanraox@users.noreply.github.com>
2024-07-28 18:12:26 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -13,7 +13,7 @@ export default withSentryConfig(nextConfig, {
 // For all available options, see:
 // https://github.com/getsentry/sentry-webpack-plugin#options
 
-org: "jsm-x9",
+org: "raox",
 project: "javascript-nextjs",
 
 // Only print logs for uploading source maps in CI
