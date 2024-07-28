48a8c7f2b420b831e0fec0d251a67b00fe553000
Aman Umrao <67886237+amanraox@users.noreply.github.com>
2024-07-28 17:28:16 +0530

    Add files via upload

---
+++ app/layout.tsx
@@ -0,0 +1,45 @@
+import { Inter as FontSans } from "next/font/google"
+
+import { cn } from "@/lib/utils"
+import './globals.css'
+import { Metadata } from "next"
+import { ClerkProvider } from "@clerk/nextjs"
+import { dark } from "@clerk/themes"
+import Provider from "./Provider"
+
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+})
+
+export const metadata: Metadata = {
+  title: 'LiveDocs',
+  description: 'Your go-to collaborative editor',
+}
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <ClerkProvider
+      appearance={{
+        baseTheme: dark,
+        variables: { 
+          colorPrimary: "#3371FF" ,
+          fontSize: '16px'
+        },
+      }}
+    >
+      <html lang="en" suppressHydrationWarning>
+        <body
+          className={cn(
+            "min-h-screen font-sans antialiased",
+            fontSans.variable
+          )}
+        >
+          <Provider>
+            {children}
+          </Provider>
+        </body>
+      </html>
+    </ClerkProvider>
+  )
+}
